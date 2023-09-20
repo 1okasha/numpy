43e394cc3294417b7a18bdc65d7fa6db1cc3a15d
1okasha <okashatariq1908@gmail.com>
2023-09-20 22:00:37 +0500

    array

---
+++ main.py
@@ -0,0 +1,43 @@
+
+#1 CALCULATOR
+
+# a=2
+# b=2
+
+# print("The value of" ,a,"+", b," is:",a+b)
+# print("The value of" ,a,"-", b," is:",a-b)
+# print("The value of" ,a,"*", b," is:",a*b)
+# print("The value of" ,a,"/", b," is:",a/b)
+# print("The value of" ,a,"**", b," is:",a**b)
+
+
+#Acc to time GM,GA,GE
+
+
+# time=int(input("TIME IS:"))
+# print(time)
+
+# if(time= 0<12):
+#     print("GOOD MORNING")
+# elif(time=12>=1||1>=5):
+#     print("GOOM AFTERNOON")    
+# else(time):
+
+
+#Q2
+O='okasha'
+print("Hello ", O ,"would you like to learn some python")
+
+#Q3
+K="OKASHA"
+print(K.lower())
+print(K.upper())
+print(K.title())
+
+#Q4
+print('\"They succeed, because they think they can.\"''-Virgil')
+
+#Q5
+famous_person="virgil"
+message='\"They succeed, because they think they can.\"'
+print(message,famous_person)
